5957d69375f5f2ff03dc90c8d9bbe51060aea510
arroyhondo <garysanchez012@gmail.com>
2021-07-19 08:27:15 -0400

    properly interfaced with the inquirer package

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require("inquirer");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-// const animalArray = ["dog", "cat", "pig"];
-
-// animalArray.push("cow");
-
-// const personObj = {
-//   name: "Gary Sanchez",
-//   age: 31,
-// };
-
-// personObj.age = 100;
-// personObj.occupation = "Developer";
-
-// //Using function expression syntax
-// const addNums = function (numOne, numTwo) {
-//     return numOne + numTwo;
-// };
-
-// //using new arrow function syntax
-// //if only performing one action, we don't need the curly braces
-// const addNum = (numOne, numTwo) => numOne + numTwo;
-// const sum = addNum(5, 3); //sum would be 8
-
-// //let is like var and will allow reassignments of values
-// let message = "Hello Node!";
-// message = "Hello ES6!";
-
-// let sums = 5 + 3;
-// sums += 1;
-
-// //dont need paranthesis around 1 paramater
-// const printProfileData = profileDataArr => {
-//   //this...
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log("====================");
-
-//   //is the same as this..
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem);
-//     //same as above just cleaner because only 1 action is being done
-//     // profileDataArr.forEach(profileItem => console.log(profileItem));
-//   });
-// };
-
-// printProfileData(profileDataArgs);
+ inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
